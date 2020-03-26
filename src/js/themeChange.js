17dab98bec50e9b4f678ebbf137ea3cf2fc9e49d
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-03-26 14:55:43 +0100

    nu virker det perfekt

--- src/js/themeChange.js
+++ src/js/themeChange.js
@@ -1,11 +1,19 @@
 let btn = document.querySelector("button");
+let check = localStorage.getItem('theme');
+
 if(btn === null){
     lightDark();
-    // window.location.reload() - find a way to run this only once
 }else{
     lightDark();
     btn.addEventListener("click", ()=>{
         lightDark();
+        if(check === "light"){
+            localStorage.setItem("theme", "dark");
+        }else if(check === "dark"){
+            localStorage.setItem("theme", "light");
+        }else{
+            localStorage.setItem("theme", "dark");
+        }
     });
 }
 function lightDark(){
@@ -14,7 +22,6 @@ function lightDark(){
     let inbox = document.querySelector(".inbox");
     let settings = document.querySelector(".settings");
     if(check === "light"){
-        localStorage.setItem("theme", "dark");
         document.documentElement.style.setProperty('--background-color', '#1B1C20');
         document.documentElement.style.setProperty('--text-color', '#fff');
 
@@ -29,7 +36,6 @@ function lightDark(){
             settings.src="assets/images/Settings-white.svg";
         }
     }else if(check === "dark"){
-        localStorage.setItem("theme", "light");
         document.documentElement.style.setProperty('--background-color', '#fff');
         document.documentElement.style.setProperty('--text-color', '#1B1C20');
 
@@ -44,7 +50,6 @@ function lightDark(){
             settings.src="assets/images/Settings-black.svg";
         }
     }else{
-        localStorage.setItem("theme", "dark");
         document.documentElement.style.setProperty('--background-color', '#1B1C20');
         document.documentElement.style.setProperty('--text-color', '#fff');
 
@@ -60,4 +65,3 @@ function lightDark(){
         }
     }
 }
-// works across pages but you have to reload first
