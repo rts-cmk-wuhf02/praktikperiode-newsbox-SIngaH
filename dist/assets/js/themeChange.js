9cc80aa8241949758624764b963d9f1b7b1f2d4c
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-03-30 11:25:52 +0200

    tilføjede domcontentloaded

--- dist/assets/js/themeChange.js
+++ dist/assets/js/themeChange.js
@@ -1,67 +1,69 @@
-let btn = document.querySelector("button");
-let check = localStorage.getItem('theme');
-
-if(btn === null){
-    lightDark();
-}else{
-    lightDark();
-    btn.addEventListener("click", ()=>{
-        lightDark();
-        if(check === "light"){
-            localStorage.setItem("theme", "dark");
-        }else if(check === "dark"){
-            localStorage.setItem("theme", "light");
-        }else{
-            localStorage.setItem("theme", "dark");
-        }
-    });
-}
-function lightDark(){
+document.addEventListener("DOMContentLoaded", ()=>{
+    let btn = document.querySelector("button");
     let check = localStorage.getItem('theme');
-    let arrow = document.querySelectorAll(".arrow");
-    let inbox = document.querySelector(".inbox");
-    let settings = document.querySelector(".settings");
-    if(check === "light"){
-        document.documentElement.style.setProperty('--background-color', '#1B1C20');
-        document.documentElement.style.setProperty('--text-color', '#fff');
 
-        for (i = 0; i < arrow.length; i++) {
-            arrow[i].src = "assets/images/arrow-white.svg";
-        }
-
-        if(inbox !== null){
-            inbox.src="assets/images/inbox-white.svg";
-            settings.src="assets/images/Settings-white.svg";
-        }else if(settings !== null){
-            settings.src="assets/images/Settings-white.svg";
-        }
-    }else if(check === "dark"){
-        document.documentElement.style.setProperty('--background-color', '#fff');
-        document.documentElement.style.setProperty('--text-color', '#1B1C20');
-
-        for (i = 0; i < arrow.length; i++) {
-            arrow[i].src = "assets/images/arrow-black.svg";
-        }
-
-        if(inbox !== null){
-            inbox.src="assets/images/inbox-black.svg";
-            settings.src="assets/images/Settings-black.svg";
-        }else if(settings !== null){
-            settings.src="assets/images/Settings-black.svg";
-        }
+    if(btn === null){
+        lightDark();
     }else{
-        document.documentElement.style.setProperty('--background-color', '#1B1C20');
-        document.documentElement.style.setProperty('--text-color', '#fff');
+        lightDark();
+        btn.addEventListener("click", ()=>{
+            lightDark();
+            if(check === "light"){
+                localStorage.setItem("theme", "dark");
+            }else if(check === "dark"){
+                localStorage.setItem("theme", "light");
+            }else{
+                localStorage.setItem("theme", "dark");
+            }
+        });
+    }
+    function lightDark(){
+        let check = localStorage.getItem('theme');
+        let arrow = document.querySelectorAll(".arrow");
+        let inbox = document.querySelector(".inbox");
+        let settings = document.querySelector(".settings");
+        if(check === "light"){
+            document.documentElement.style.setProperty('--background-color', '#1B1C20');
+            document.documentElement.style.setProperty('--text-color', '#fff');
 
-        for (i = 0; i < arrow.length; i++) {
-            arrow[i].src = "assets/images/arrow-white.svg";
-        }
+            for (i = 0; i < arrow.length; i++) {
+                arrow[i].src = "assets/images/arrow-white.svg";
+            }
 
-        if(inbox !== null){
-            inbox.src="assets/images/inbox-white.svg";
-            settings.src="assets/images/Settings-white.svg";
-        }else if(settings !== null){
-            settings.src="assets/images/Settings-white.svg";
+            if(inbox !== null){
+                inbox.src="assets/images/inbox-white.svg";
+                settings.src="assets/images/Settings-white.svg";
+            }else if(settings !== null){
+                settings.src="assets/images/Settings-white.svg";
+            }
+        }else if(check === "dark"){
+            document.documentElement.style.setProperty('--background-color', '#fff');
+            document.documentElement.style.setProperty('--text-color', '#1B1C20');
+
+            for (i = 0; i < arrow.length; i++) {
+                arrow[i].src = "assets/images/arrow-black.svg";
+            }
+
+            if(inbox !== null){
+                inbox.src="assets/images/inbox-black.svg";
+                settings.src="assets/images/Settings-black.svg";
+            }else if(settings !== null){
+                settings.src="assets/images/Settings-black.svg";
+            }
+        }else{
+            document.documentElement.style.setProperty('--background-color', '#1B1C20');
+            document.documentElement.style.setProperty('--text-color', '#fff');
+
+            for (i = 0; i < arrow.length; i++) {
+                arrow[i].src = "assets/images/arrow-white.svg";
+            }
+
+            if(inbox !== null){
+                inbox.src="assets/images/inbox-white.svg";
+                settings.src="assets/images/Settings-white.svg";
+            }else if(settings !== null){
+                settings.src="assets/images/Settings-white.svg";
+            }
         }
     }
-}
+});
