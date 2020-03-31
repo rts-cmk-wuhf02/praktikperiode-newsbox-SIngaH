5d5fb776b2bd2a977c41ea8bfe4c5c95dbefe1e3
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-03-31 08:34:02 +0200

    ordnede et lille problem (category navnet blev fjernet da man chlickede på den)

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let dropDownNews = document.querySelectorAll(".category-top");
     let dropDownArray = Array.from(dropDownNews);
-    let arrow = document.querySelector(".category-arrow");
+    // let arrow = document.querySelector(".category-arrow");
     
     for (i = 0; i < dropDownArray.length; i++) {
         dropDownArray[i].addEventListener("click", arrowFunction);   
@@ -9,7 +9,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     function arrowFunction(e){
         let next = e.target.parentElement.children[1];
-        next.classList.toggle("hide")
+        //the healine disappeared when clicked
+        let headline = e.target.parentElement.children[0].children[0].children[1];
+        headline.classList.remove("hide");
+        next.classList.toggle("hide");        
         let arrow = e.target.children[1];
         arrow.classList.toggle("arrow-down");
     }
