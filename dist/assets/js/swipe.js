9bff2f20f6b1cdf1cbb1f06d6e80f69015ef75c2
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-15 12:47:34 +0200

    swipe attempt

---
+++ dist/assets/js/swipe.js
@@ -0,0 +1,10 @@
+          //swipe
+        //   function swipe(event){
+        //     anime({
+        //         targets: '.news',
+        //         translateX: -100,
+        //         easing: 'easeInOutQuad',
+        //         duration: 3000,
+        //         direction: 'reverse'
+        //       });
+        //     }
