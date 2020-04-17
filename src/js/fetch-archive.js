6fddd6f06c824ad908f8cbe828e7daf10b809ea2
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-17 12:32:43 +0200

    forkortede koden

--- src/js/fetch-archive.js
+++ src/js/fetch-archive.js
@@ -154,40 +154,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 </div>
             </div>
             `
-            //swipe
-            const news = document.querySelectorAll('.news');
-            const newsArray = Array.from(news);
-            newsArray.forEach(_C =>{
-              N = _C.children.length;
-  
-              _C.style.setProperty('--n', N)
-              
-              let x0 = null;
-  
-              _C.addEventListener('mousedown', lock, false);
-              _C.addEventListener('touchstart', lock, false);
-  
-              _C.addEventListener('mouseup', move, false);
-              _C.addEventListener('touchend', move, false);
-  
-              function lock(e) { x0 = unify(e).clientX };
-              
-              let i = 0;
-  
-              function move(e) {
-                if(x0 || x0 === 0) {
-                  let dx = unify(e).clientX - x0, s = Math.sign(dx);
-  
-                  if((i > 0 || s < 0) && (i < N - 1 || s > 0))
-                  _C.style.setProperty('--i', i -= s);
-  
-                  x0 = null
-                }
-              };
-              _C.addEventListener('touchmove', e => {e.preventDefault()}, false)
-  
-              function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
-            });
+
+            swipe();
                     
             //remove from archives
             let trash = document.querySelectorAll(".trash");
@@ -243,40 +211,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
             </div>
             `
 
-            //swipe
-            const news = document.querySelectorAll('.news');
-            const newsArray = Array.from(news);
-            newsArray.forEach(_C =>{
-              N = _C.children.length;
-  
-              _C.style.setProperty('--n', N)
-              
-              let x0 = null;
-  
-              _C.addEventListener('mousedown', lock, false);
-              _C.addEventListener('touchstart', lock, false);
-  
-              _C.addEventListener('mouseup', move, false);
-              _C.addEventListener('touchend', move, false);
-  
-              function lock(e) { x0 = unify(e).clientX };
-              
-              let i = 0;
-  
-              function move(e) {
-                if(x0 || x0 === 0) {
-                  let dx = unify(e).clientX - x0, s = Math.sign(dx);
-  
-                  if((i > 0 || s < 0) && (i < N - 1 || s > 0))
-                  _C.style.setProperty('--i', i -= s);
-  
-                  x0 = null
-                }
-              };
-              _C.addEventListener('touchmove', e => {e.preventDefault()}, false)
-  
-              function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
-            });
+            swipe();
                     
             //remove from archives
             let trash = document.querySelectorAll(".trash");
@@ -332,41 +267,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
             </div>
             `
 
-            //swipe
-            const news = document.querySelectorAll('.news');
-            const newsArray = Array.from(news);
-            newsArray.forEach(_C =>{
-              N = _C.children.length;
-  
-              _C.style.setProperty('--n', N)
+            swipe();
               
-              let x0 = null;
-  
-              _C.addEventListener('mousedown', lock, false);
-              _C.addEventListener('touchstart', lock, false);
-  
-              _C.addEventListener('mouseup', move, false);
-              _C.addEventListener('touchend', move, false);
-  
-              function lock(e) { x0 = unify(e).clientX };
-              
-              let i = 0;
-  
-              function move(e) {
-                if(x0 || x0 === 0) {
-                  let dx = unify(e).clientX - x0, s = Math.sign(dx);
-  
-                  if((i > 0 || s < 0) && (i < N - 1 || s > 0))
-                  _C.style.setProperty('--i', i -= s);
-  
-                  x0 = null
-                }
-              };
-              _C.addEventListener('touchmove', e => {e.preventDefault()}, false)
-  
-              function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
-            });
-                    
             //remove from archives
             let trash = document.querySelectorAll(".trash");
             let trashArray = Array.from(trash);
@@ -420,41 +322,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 </div>
             </div>
             `
-            
-            //swipe
-            const news = document.querySelectorAll('.news');
-            const newsArray = Array.from(news);
-            newsArray.forEach(_C =>{
-              N = _C.children.length;
-  
-              _C.style.setProperty('--n', N)
-              
-              let x0 = null;
-  
-              _C.addEventListener('mousedown', lock, false);
-              _C.addEventListener('touchstart', lock, false);
-  
-              _C.addEventListener('mouseup', move, false);
-              _C.addEventListener('touchend', move, false);
-  
-              function lock(e) { x0 = unify(e).clientX };
-              
-              let i = 0;
-  
-              function move(e) {
-                if(x0 || x0 === 0) {
-                  let dx = unify(e).clientX - x0, s = Math.sign(dx);
-  
-                  if((i > 0 || s < 0) && (i < N - 1 || s > 0))
-                  _C.style.setProperty('--i', i -= s);
-  
-                  x0 = null
-                }
-              };
-              _C.addEventListener('touchmove', e => {e.preventDefault()}, false)
-  
-              function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
-            });
+
+            swipe();
                     
             //remove from archives
             let trash = document.querySelectorAll(".trash");
@@ -506,5 +375,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   
     return jsonResult;
+  }
+
+  //swipe
+  function swipe(){
+    const news = document.querySelectorAll('.news');
+    const newsArray = Array.from(news);
+    newsArray.forEach(_C =>{
+      N = _C.children.length;
+    
+      _C.style.setProperty('--n', N)
+      
+      let x0 = null;
+    
+      _C.addEventListener('mousedown', lock, false);
+      _C.addEventListener('touchstart', lock, false);
+    
+      _C.addEventListener('mouseup', move, false);
+      _C.addEventListener('touchend', move, false);
+    
+      function lock(e) { x0 = unify(e).clientX };
+      
+      let i = 0;
+    
+      function move(e) {
+        if(x0 || x0 === 0) {
+          let dx = unify(e).clientX - x0, s = Math.sign(dx);
+        
+          if((i > 0 || s < 0) && (i < N - 1 || s > 0))
+          _C.style.setProperty('--i', i -= s);
+        
+          x0 = null
+        }
+      };
+      _C.addEventListener('touchmove', e => {e.preventDefault()}, false)
+    
+      function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
+    });
   }
 });  
